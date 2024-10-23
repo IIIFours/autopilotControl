0f290b97ca8dd97f88b3a68405c77c9ec666a992
Alexander Schlake <schlake@3fours.dev>
2024-10-23 14:01:43 -0500

    Initial Commit

---
+++ autopilotControl/autopilotControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  autopilotControlApp.swift
+//  autopilotControl
+//
+//  Created by Alexander Schlake on 10/23/24.
+//
+
+import SwiftUI
+
+@main
+struct autopilotControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ autopilotControlTests/autopilotControlTests.swift
@@ -0,0 +1,17 @@
+//
+//  autopilotControlTests.swift
+//  autopilotControlTests
+//
+//  Created by Alexander Schlake on 10/23/24.
+//
+
+import Testing
+@testable import autopilotControl
+
+struct autopilotControlTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
