a3941832cb283b3f07d44e06696a0cc3570a851e
Alexander Schlake <schlake@3fours.dev>
2024-10-23 23:20:45 -0500

    Polished UI, writing new PID values to BLE characteristic

--- autopilotControl/BLEManager.swift
+++ autopilotControl/BLEManager.swift
@@ -32,28 +32,74 @@ struct Autopilot: CustomStringConvertible {
     var homingComplete: Bool
     
     var description: String {
-            return """
-            kp: \(kp)
-            ki: \(ki)
-            kd: \(kd)
-            bearingPositionToDestinationWaypoint: \(bearingPositionToDestinationWaypoint)
-            destinationLatitude: \(destinationLatitude)
-            destinationLongitude: \(destinationLongitude)
-            previousDestinationLatitude: \(previousDestinationLatitude)
-            previousDestinationLongitude: \(previousDestinationLongitude)
-            heading: \(heading)
-            xte: \(xte)
-            previousXte: \(previousXte)
-            previousTime: \(previousTime)
-            previousBearing: \(previousBearing)
-            integralXTE: \(integralXTE)
-            derivativeXTE: \(derivativeXTE)
-            timeDelta: \(timeDelta)
-            rudderAngle: \(rudderAngle)
-            rudderPosition: \(rudderPosition)
-            targetMotorPosition: \(targetMotorPosition)
-            homingComplete: \(homingComplete)
-            """
+        return """
+        P: \(kp)
+        I: \(ki)
+        D: \(kd)
+        Bearing: \(bearingPositionToDestinationWaypoint)
+        Destination Latitude: \(destinationLatitude)
+        Destination Longitude: \(destinationLongitude)
+        Previous Destination Latitude: \(previousDestinationLatitude)
+        Previous Destination Longitude: \(previousDestinationLongitude)
+        Heading: \(heading)
+        Cross-track Error: \(xte)
+        Previous Cross-track Error: \(previousXte)
+        Previous Time: \(previousTime)
+        Previous Bearing: \(previousBearing)
+        Integral Cross-track Error: \(integralXTE)
+        Derivative Cross-track Error: \(derivativeXTE)
+        Time Delta: \(timeDelta)
+        Rudder Angle: \(rudderAngle)
+        Rudder Position: \(rudderPosition)
+        Target Motor Position: \(targetMotorPosition)
+        Homing Complete: \(homingComplete)
+        """
+    }
+    
+    var kpDescription: String { "Proportional Gain (Kp): \(kp)" }
+    var kiDescription: String { "Integral Gain (Ki): \(ki)" }
+    var kdDescription: String { "Derivative Gain (Kd): \(kd)" }
+    var bearingDescription: String { "Bearing to Destination Waypoint: \(bearingPositionToDestinationWaypoint)" }
+    var destinationLatitudeDescription: String { "Destination Latitude: \(destinationLatitude)" }
+    var destinationLongitudeDescription: String { "Destination Longitude: \(destinationLongitude)" }
+    var previousDestinationLatitudeDescription: String { "Previous Destination Latitude: \(previousDestinationLatitude)" }
+    var previousDestinationLongitudeDescription: String { "Previous Destination Longitude: \(previousDestinationLongitude)" }
+    var headingDescription: String { "Current Heading: \(heading)" }
+    var xteDescription: String { "Cross-Track Error (XTE): \(xte)" }
+    var previousXteDescription: String { "Previous Cross-Track Error (Previous XTE): \(previousXte)" }
+    var previousTimeDescription: String { "Previous Time: \(previousTime)" }
+    var previousBearingDescription: String { "Previous Bearing: \(previousBearing)" }
+    var integralXTEDescription: String { "Integral of Cross-Track Error: \(integralXTE)" }
+    var derivativeXTEDescription: String { "Derivative of Cross-Track Error: \(derivativeXTE)" }
+    var timeDeltaDescription: String { "Time Delta: \(timeDelta)" }
+    var rudderAngleDescription: String { "Rudder Angle (Degrees): \(rudderAngle)" }
+    var rudderPositionDescription: String { "Rudder Position: \(rudderPosition)" }
+    var targetMotorPositionDescription: String { "Target Motor Position: \(targetMotorPosition)" }
+    var homingCompleteDescription: String { "Homing Complete: \(homingComplete ? "Yes" : "No")" }
+    
+    func allDescriptions() -> [String] {
+            return [
+                kpDescription,
+                kiDescription,
+                kdDescription,
+                bearingDescription,
+                destinationLatitudeDescription,
+                destinationLongitudeDescription,
+                previousDestinationLatitudeDescription,
+                previousDestinationLongitudeDescription,
+                headingDescription,
+                xteDescription,
+                previousXteDescription,
+                previousTimeDescription,
+                previousBearingDescription,
+                integralXTEDescription,
+                derivativeXTEDescription,
+                timeDeltaDescription,
+                rudderAngleDescription,
+                rudderPositionDescription,
+                targetMotorPositionDescription,
+                homingCompleteDescription
+            ]
         }
 }
 
@@ -98,10 +144,26 @@ func deserializeAutopilot(data: Data) -> Autopilot {
     )
 }
 
+extension Autopilot {
+    func toData() -> Data {
+        var copy = self
+        return withUnsafeBytes(of: &copy) { Data($0) }
+    }
+}
+
+extension Float {
+    // Rounds the float to the specified number of decimal places
+    func rounded(toPlaces places: Int) -> Float {
+        let divisor = pow(10.0, Float(places))
+        return (self * divisor).rounded() / divisor
+    }
+}
+
 // BLE Manager that handles Bluetooth connections and publishes changes to SwiftUI views
 class BLEManager: NSObject, ObservableObject, CBCentralManagerDelegate, CBPeripheralDelegate {
     var centralManager: CBCentralManager!
     var connectedPeripheral: CBPeripheral?
+    var targetCharacteristic: CBCharacteristic?
     
     @Published var isConnected = false
     @Published var autopilot: Autopilot = Autopilot(
@@ -134,9 +196,7 @@ class BLEManager: NSObject, ObservableObject, CBCentralManagerDelegate, CBPeriph
         super.init()
         centralManager = CBCentralManager(delegate: self, queue: nil)
     }
-    
-    // MARK: - Central Manager Delegate
-    
+        
     func centralManagerDidUpdateState(_ central: CBCentralManager) {
         if central.state == .poweredOn {
             // Start scanning for peripherals with the target service UUID
@@ -178,6 +238,7 @@ class BLEManager: NSObject, ObservableObject, CBCentralManagerDelegate, CBPeriph
                 if characteristic.uuid == targetCharacteristicUUID {
                     peripheral.readValue(for: characteristic)
                     peripheral.setNotifyValue(true, for: characteristic)
+                    targetCharacteristic = characteristic
                 }
             }
         }
@@ -190,5 +251,19 @@ class BLEManager: NSObject, ObservableObject, CBCentralManagerDelegate, CBPeriph
                 self.autopilot = autopilot;
             }
         }
+    }
+    
+    func writeToCharacteristic(autopilot: Autopilot) {
+        if let peripheral = connectedPeripheral, let characteristic = targetCharacteristic {
+            let data = autopilot.toData()  // Serialize the Autopilot struct to Data
+            peripheral.writeValue(data, for: characteristic, type: .withResponse)  // Write the data to the characteristic
+        }
+    }
+    
+    func sendUpdatedPIDValues(kpInput: Float, kiInput: Float, kdInput: Float) {
+        autopilot.kp = kpInput.rounded(toPlaces: 1);
+        autopilot.ki = kiInput.rounded(toPlaces: 1);
+        autopilot.kd = kdInput.rounded(toPlaces: 1);
+        writeToCharacteristic(autopilot: autopilot);
     }
 }
--- autopilotControl/ContentView.swift
+++ autopilotControl/ContentView.swift
@@ -9,25 +9,98 @@ import SwiftUI
 
 struct ContentView: View {
     @ObservedObject var bleManager = BLEManager();
+    
+    @State private var kpInput: Float = 0.0;
+    @State private var kiInput: Float = 0.0;
+    @State private var kdInput: Float = 0.0;
+    @State private var valuesInitialized = false;
         
-        var body: some View {
-            VStack {
-                if bleManager.isConnected {
-                    Text("Connected to Autopilot")
-                        .foregroundColor(.green)
+    var body: some View {
+        NavigationView {
+            VStack() {
+                if bleManager.isConnected && bleManager.autopilot.kp != 0.0 && bleManager.autopilot.ki != 0.0 && bleManager.autopilot.kd != 0.0 {
+                    VStack() {
+                        HStack {
+                            Label {
+                                VStack(alignment: .leading) {
+                                    Text("Proportional")
+                                }
+                            } icon: {
+                                Image(systemName: "angle")
+                            }
+                            Stepper("", value: $kpInput, in: 0...10, step: 0.1)
+                            Text("\(String(format: "%.1f", kpInput))")
+                        }.padding(.bottom)
+                        
+                        HStack {
+                            Label {
+                                VStack(alignment: .leading) {
+                                    Text("Integral")
+                                }
+                            } icon: {
+                                Image(systemName: "sum")
+                            }
+                            Spacer()
+                            Stepper("", value: $kiInput, in: 0...10, step: 0.1)
+                            Text("\(String(format: "%.1f", kiInput))")
+                        }.padding(.bottom)
+                        
+                        HStack {
+                            Label {
+                                VStack(alignment: .leading) {
+                                    Text("Derivative")
+                                }
+                            } icon: {
+                                Image(systemName: "function")
+                            }
+                            Spacer()
+                            Stepper("", value: $kdInput, in: 0...10, step: 0.1)
+                            Text("\(String(format: "%.1f", kdInput))")
+                        }.padding(.bottom)
+                        
+                        Button(action: {
+                            bleManager.sendUpdatedPIDValues(kpInput: kpInput, kiInput: kiInput, kdInput: kdInput);
+                        }) {
+                            Text("Update Gains")
+                                .padding()
+                                .background(Color.teal)
+                                .foregroundColor(.black)
+                                .cornerRadius(10)
+                        }.padding(.bottom)
+                        HStack {
+                            Label {
+                                VStack(alignment: .leading) {
+                                    Text("Active")
+                                }
+                            } icon: {
+                                Image(systemName: "clock")
+                            }
+                            Text("\(String(format: "%.fmin", (bleManager.autopilot.previousTime/1000)/60))")
+                        }.padding(.bottom)
+                        List {
+                            ForEach(bleManager.autopilot.allDescriptions(), id: \.self) { description in
+                                Text(description)
+                                    .font(.body)
+                            }
+                        }
+                    }.onAppear {
+                        if !valuesInitialized {
+                            kpInput = bleManager.autopilot.kp;
+                            kiInput = bleManager.autopilot.ki;
+                            kdInput = bleManager.autopilot.kd;
+                            self.valuesInitialized = true;
+                        }
+                    }
                 } else {
-                    Text("Connecting...")
-                        .foregroundColor(.red)
+                    Text("Waiting for connection...")
+                    ProgressView()
+                        .progressViewStyle(CircularProgressViewStyle())
                 }
-                
-                Text(String(describing: bleManager.autopilot))
-                    .font(.body)
-                    .padding()
-                
-                Spacer()
             }
             .padding()
+            .navigationTitle("Autopilot Control")
         }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
